1acdf7e1ecad8888d58dea6f9a8c00709f7f1cf0
EvilGeorge666 <97815658+EvilGeorge666@users.noreply.github.com>
2025-03-03 13:04:10 -0600

    Update algo.hpp

--- algorithms/include/algo.hpp
+++ algorithms/include/algo.hpp
@@ -5,17 +5,52 @@
 
 long fibonacci(const int n)
 {
-    return 0;
+    long fibAns = 2;
+    long prevNum = 1;
+
+    if(n <= 3)
+    {
+        fibAns = n - 1;
+    }
+    else
+    {
+        for(int i = 0; i < n - 3; i++)
+        {
+            long temp = fibAns + prevNum;
+            prevNum = fibAns;
+            fibAns = temp;
+        }
+    }
+
+    return fibAns;
 }
 
 int linear_search(Array * a, const int target)
 {
-    return 0;
+    int idx = 0;
+    for (size_t i = 0; i < a->len; i++) 
+    {
+        int elem = a->data[i];
+        if (elem == target)
+        {
+            return idx; 
+        }
+        idx++;
+    }
+    return -1;
 }
 
 long factorial(const int n)
 {
-    return 0;
+    long factAns = 1;
+    int i;
+    
+    for(i = 1; i <= n; i++)
+    {
+        factAns *= i;
+    }
+
+    return factAns;
 }
 
 #endif // ALGO_H_
