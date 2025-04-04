73ebd32bdc8f4776be6c749b5a9d6362c493a1de
EvilGeorge666 <97815658+EvilGeorge666@users.noreply.github.com>
2025-04-04 15:57:42 -0500

    Update user_solver.cpp

--- battleship/src/user_solver.cpp
+++ battleship/src/user_solver.cpp
@@ -2,7 +2,19 @@
 #include "../include/user_solver.h"
 
 void UserSolver::solve(Board &board) {
-    // Your Implementation goes here
-    ReturnType t = guess(1, 1, board);
-    std::cout << t << std::endl;
+    //used to track how many ships you hit
+    size_t hitCounter = 0;
+
+    //Loop through the entire board until you hit all the ships
+    for (size_t i = 0; i < board.getX() && hitCounter < board.getShipCount(); i++) {
+        for (size_t j = 0; j < board.getY() && hitCounter < board.getShipCount(); j++) {
+            // if there is a hit, add to the hit counter
+            if (guess(i, j, board) == hit)
+            {
+                hitCounter++;
+            }
+        }
+    }
+    
+
 }
