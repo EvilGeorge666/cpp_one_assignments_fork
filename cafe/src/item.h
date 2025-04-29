f7eae84fb25358af6aec1744db4067270768e74e
EvilGeorge666 <97815658+EvilGeorge666@users.noreply.github.com>
2025-04-28 19:58:03 -0500

    Update item.h

--- cafe/src/item.h
+++ cafe/src/item.h
@@ -5,12 +5,15 @@
 
 #include <string>
 
+enum ItemType{ COFFEE = 0, BAGEL = 1, CHIPS = 2, YOGURT = 3, JUICE = 4 };
+
 class Item {
    public:
+    ItemType type;
     float price;
     std::string description;
 
-    Item(const float &price, const std::string &description) {
+    Item(ItemType type, const float &price, const std::string &description) {
         this->price = price;
         this->description = Utils::to_lower(description);
     };
